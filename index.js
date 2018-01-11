d816733b6445406837043074f7a4f3a60eff0f30
Serban Blebea <slabestecuserban@gmail.com>
2018-01-11 23:02:48 +0000

    add heroku integration

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ const Shopify = require('shopify-api-node');
 
 const app = express();
 
+// Get configs from .env file
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const httpTunnel = process.env.HTTP_TUNNEL;
 const token = process.env.SHOPIFY_TOKEN;
+const port = process.env.PORT || 3000;
 
+// Create the callback url for authentication
 const callback = httpTunnel + '/callback';
 
+// Create the config object
 const configShop = {
     shop: 'mihaidev', // MYSHOP.myshopify.com
     shopify_api_key: apiKey, // Your API key
@@ -27,20 +31,29 @@ const configShop = {
     nonce: 'ceva007' // nonce; you must provide a randomly selected value unique for each authorization request
 }
 
+// Create the Shopify API object
 var shopAPI = new shopifyAPI(configShop);
 
+// Middleware setup
 app.use(express.static(__dirname + '/admin/build'));
+app.use((request, response, next)=> {
+    var now = new Date().toString();
+    console.log(`${now}: ${request.method} ${request.url}`);
+    next();
+})
 
+// App router setup
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'admin', 'build', 'index.html'));
 });
 
-// ** Install routes ** //
+// ** Install app route ** //
 app.get('/shopify', (request, response)=> {
     var auth_url = shopAPI.buildAuthURL();
     response.redirect(auth_url);
 })
 
+// ** THe callback that receives the token after auth ** //
 app.get('/callback', (request, response)=> {
     query_params = request.query;
 
@@ -51,6 +64,7 @@ app.get('/callback', (request, response)=> {
     });
 })
 
+// ** Get products from store ** //
 app.get('/products', (request, response)=> {
     const shop = new Shopify({
         shopName: 'mihaidev',
@@ -61,7 +75,7 @@ app.get('/products', (request, response)=> {
 })
 
 
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+// Instruct app to listen to port
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`);
 });
